8b03b9d9e6f8d2b09ccadd19aa4dcb1ada388dfc
Jordan Begg <jordansbegg@outlook.com>
2021-12-26 11:11:09 +0000

    Change calculation start date

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,10 +6,15 @@ from wtforms.validators import DataRequired
 class TripForm(FlaskForm):
     start = DateField("Start Date", validators=[DataRequired()])
     end = DateField("End Date", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    submit_add_trip = SubmitField("Submit")
 
 
 class EditProfileForm(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired()])
     surname = StringField("Surname", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    submit_edit_profile = SubmitField("Submit")
+
+
+class EndDateForm(FlaskForm):
+    end = DateField("Calculation End Date", validators=[DataRequired()])
+    submit_update_end_date = SubmitField("Submit")
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,24 +1,40 @@
 from flask_login import current_user, login_required
 from flask import flash, request, redirect, url_for, current_app, render_template
+import datetime
 
 from app import db
 from app.main import bp
-from app.main.forms import TripForm, EditProfileForm
+from app.main.forms import TripForm, EditProfileForm, EndDateForm
+from app.main.panels import EditProfilePanel
 from app.models import Trip, User
 
 
-@bp.route("/", methods=["GET", "POST"])
-@bp.route("/index", methods=["GET", "POST"])
+@bp.route(
+    "/", methods=["GET", "POST"], defaults={"date": datetime.date.today().isoformat()}
+)
+@bp.route(
+    "/index",
+    methods=["GET", "POST"],
+    defaults={"date": datetime.date.today().isoformat()},
+)
+@bp.route(
+    "/index/<date>",
+    methods=["GET", "POST"],
+)
 @login_required
-def index():
-    remaining_days = str(current_user.get_remaining_days())
+def index(date):
+    date = datetime.date.fromisoformat(date)
+    remaining_days = str(current_user.get_remaining_days(date))
     form = TripForm()
+    end_date_form = EndDateForm()
     if form.validate_on_submit():
         trip = Trip(start=form.start.data, end=form.end.data, traveller=current_user)
         db.session.add(trip)
         db.session.commit()
         flash("Trip added!")
         return redirect(url_for("main.index"))
+    if end_date_form.validate_on_submit():
+        return redirect(url_for("main.index", date=end_date_form.end.data))
     page = request.args.get("page", 1, type=int)
     trips = current_user.trips.order_by(Trip.start.desc()).paginate(
         page, current_app.config["TRIPS_PER_PAGE"], False
@@ -29,10 +45,12 @@ def index():
         "index.html",
         title="Home",
         remaining_days=remaining_days,
+        end_date_form=end_date_form,
         form=form,
         trips=trips.items,
         next_url=next_url,
         prev_url=prev_url,
+        date=date,
     )
 
 
@@ -66,14 +84,25 @@ def user(id):
 @bp.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
-    if form.validate_on_submit():
-        current_user.first_name = form.first_name.data
-        current_user.surname = form.surname.data
-        db.session.commit()
-        flash("Your changes have been saved.")
-        return redirect(url_for("main.edit_profile"))
+    panel = EditProfilePanel(current_user)
+    # form = EditProfileForm()
+    if request.method == "POST":
+        if (
+            request.form.get("submit_edit_profile")
+            and panel.edit_profile_form.validate()
+        ):
+            current_user.first_name = panel.edit_profile_form.first_name.data
+            current_user.surname = panel.edit_profile_form.surname.data
+            db.session.commit()
+            flash("Your changes have been saved.")
+            return redirect(url_for("main.edit_profile"))
+    # if form.validate_on_submit():
+    #     current_user.first_name = form.first_name.data
+    #     current_user.surname = form.surname.data
+    #     db.session.commit()
+    #     flash("Your changes have been saved.")
+    #     return redirect(url_for("main.edit_profile"))
     elif request.method == "GET":
-        form.first_name.data = current_user.first_name
-        form.surname.data = current_user.surname
-    return render_template("edit_profile.html", title="Edit Profile", form=form)
+        panel.edit_profile_form.first_name.data = current_user.first_name
+        panel.edit_profile_form.surname.data = current_user.surname
+    return render_template("edit_profile.html", title="Edit Profile", panel=panel)
